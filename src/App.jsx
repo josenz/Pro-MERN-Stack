2fa5b96e8dd59455ba0a940638b72d059c7ec82a
josenz <jose.santos.soares@hotmail.com>
2021-10-03 10:22:52 +0900

    Added babel-cli & babel-preset-react and also the JSX script was separated from the index.html

---
+++ src/App.jsx
@@ -0,0 +1,5 @@
+var contentNode = document.getElementById('contents');
+
+var component = <h1>Hello World!</h1>; // A simple JSX component
+
+ReactDOM.render(component, contentNode); // Render the component inside the content Node
